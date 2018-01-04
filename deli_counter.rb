35aee6ddddf8532d0c6dfbc63af8c95525ee99c6
Darren A Praseutsinh <dapraseutsinh@gmail.com>
2018-01-04 11:39:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,8 @@ def line
   if katz_deli.length == 0 
     puts "The line is currently empty."
   else 
-    katz_deli.each_with_index(1)
+    katz_deli.each.with_index(1) do | name, idx |
+      puts " "
+    end 
   end 
 end 
