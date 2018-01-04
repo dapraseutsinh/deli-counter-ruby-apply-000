b3d8676197af656c50ec7847b55af22cfa7da377
Darren A Praseutsinh <dapraseutsinh@gmail.com>
2018-01-04 12:06:38 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,7 @@
-katz_deli = []
+katz_deli =[]
 
 def line(katz_deli)
-  other_deli =[]
   if katz_deli.length == 0 
     puts "The line is currently empty."
-  else 
-    katz_deli.each.with_index(1) do |name, idx|
-      other_deli << "#{idx}. #{name}"
-    end 
-    puts "The line is currently: " + other_deli.join(" ")
   end 
 end 
